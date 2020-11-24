bcd4934b007a2c4ab727225b3602f487890f703d
Chris Kottisse <chris.kottisse@khk.ee>
2020-11-24 09:46:17 +0200

    ül 2 matemaatika

---
+++ ülesanded/yl2.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+    matemaatika
+    ül 2
+    Chris Kottisse
+    24.11
+*/
+
+$x = 21;
+$z = 12;
+
+echo "$x + $z = ",$x + $z, "<br>";
+echo "$x + $z = ",$x + $z, '<br>';
+echo "$x * $z = ",$x * $z, "<br>";
+echo "$x / $z = ",$x / $z, "<br>";
+echo "$x % $z = ",$x % $z, "<br>";
+
+$teiearv = 2;
+$convert = $teiearv*1000;
+
+echo "Teie ARV ON ",$teiearv,'. Kuid teie vastus on ',$convert;
+
+
+?>
