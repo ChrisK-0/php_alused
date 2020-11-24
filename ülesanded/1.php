5de8598ac828f30dbc5b612b1b751599ce3e54d0
Chris Kottisse <chris.kottisse@khk.ee>
2020-11-24 09:10:24 +0200

    ül 1 muutujad

---
+++ ülesanded/1.php
@@ -0,0 +1,21 @@
+<?php
+/*
+    Muutujad
+    ül 1
+    Chris Kottisse
+    24.11
+*/
+$m1 = "Chris";
+$m2 = 2000;
+$m3 = "skorpion";
+$m4 = '“It’s My Life” – Dr. Alban';
+
+    echo $m1," on äge ning aastla ",$m2," tegi ta ellu ärkamist ja ",$m3," temast sai. <br>";
+
+echo $m4,"<br>","<br>","<br>","<br>","<br>","<br>";
+
+echo "(\(\ <br>";
+echo "( -.-) <br>";
+echo "o_('')('') <br>";
+
+?>
