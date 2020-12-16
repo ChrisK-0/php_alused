7e8d2cc7cec0491b3f7fcb75086380477915d1ed
Chris Kottisse <chris.kottisse@khk.ee>
2020-12-16 15:46:31 +0200

    yl 9

--- ülesanded/yl9.php
+++ ülesanded/yl9.php
@@ -33,7 +33,6 @@ echo '<!DOCTYPE html>
     <title>Pannkoogi klubi</title>
     <meta charset="UTF-8">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 </head>
 <body>
 <div class="container">
@@ -42,25 +41,51 @@ echo '<!DOCTYPE html>
             <label for="kasutaja">Username</label>
             <input type="text" class="form-control" id="kasutaja" placeholder="Kasutajanimi" name="kasutaja">
         </div>
+        <div class="form-group">
+            <label for="exampleFormControlTextarea1">Teksti tabel</label>
+            <input type="text" class="form-control" id="text_" placeholder="Tekst" name="text_">
+        </div>
         <button type="submit" class="btn btn-primary">OK</button>
     </form>
 </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 </body>
 </html>';
-$kasutaja = "Kasutaja";
+// kasutajanime echo
+$kasutaja = "kasutaja";
 $kasutaja = $_GET['kasutaja'];
 $alert_kasutaja = ucfirst(strtolower($kasutaja));
+if(isset($alert_kasutaja)){
+    $kasutaja = "Kasutaja";
+}
+
+// teksti tabeli echo
+$text_ = $_GET['text_'];
+$text_alert_ = ucfirst(strtolower($text_));
+
+// halbade s6nade vahendus
+$filter_list_ = array("Noob", "noob", "kurat", "Kurat");
+$replace_ = "***";
+$text_filtering = str_replace($filter_list_, $replace_, $text_alert_);
+
+
+// v2ljundid
+
 echo 'Tere tulemast '.$alert_kasutaja.'.';
-
 echo '<br>';
+echo $text_filtering.'.';
 
+
+echo '<br><br><br>';
+
+// stalkeri tykeldamine
 $word = "stalker";
 $chars = preg_split('//', $word, -1, PREG_SPLIT_NO_EMPTY);
 print_r($chars);
-
+echo '<br><br>';
 for($nr=1;$nr<=10;$nr++){
     if(strlen($word)<13){
-        $chars
+        $chars;
     }
     echo $nr.'<br>';
 }
